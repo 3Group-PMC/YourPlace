8225d27f6af2d08fcb583814a09742d7a101dd50
dalthviz <d.althviz10@uniandes.edu.co>
2016-11-15 19:20:51 -0500

    Added toggling of visibility of the layers

--- app/app.js
+++ app/app.js
@@ -14,21 +14,26 @@ require([
     "esri/dijit/Search",
     "esri/dijit/Popup",
     "esri/dijit/PopupTemplate",
-    "esri/config",
+    "esri/config","esri/layers/CSVLayer",
+    "esri/Color",
+    "esri/symbols/SimpleMarkerSymbol",
+    "esri/renderers/SimpleRenderer",
+    "esri/InfoTemplate",
     // Bootstrap
     "bootstrap/Collapse",
 
     // Calcite-maps
     "calcite-maps/calcitemaps-v0.2",
     "dojo/domReady!"
-], function( Map, FeatureLayer, TileLayer, WMSLayer, query, Search,Popup,PopupTemplate, config) {
+], function( Map, FeatureLayer, TileLayer, WMSLayer, query, Search,Popup,PopupTemplate, config,
+             CSVLayer, Color, SimpleMarkerSymbol, SimpleRenderer, InfoTemplate) {
 
-    config.defaults.io.corsEnabledServers.push("http://serviciosgis.catastrobogota.gov.co/");
+    config.defaults.io.corsEnabledServers.push("drive.google.com");
 
 
     // Map
     var map = new Map("mapViewDiv",{
-        basemap: "streets",
+        basemap: "osm",
         padding: {
             top: 50
         },
@@ -39,53 +44,73 @@ require([
 
 
     //add layers
+
+    //Schools Bogota
+    var fl = new FeatureLayer("https://services2.arcgis.com/NFAaR7TquHkVlqVD/arcgis/rest/services/Colegios_Bogota/FeatureServer/0",{
+        outFields : ["*"],
+        infoTemplate: template
+    });
+
+
+    //Density of population
     var fl2 = new TileLayer("https://tiles.arcgis.com/tiles/4yjifSiIG17X0gW4/arcgis/rest/services/PopulationDensity_Bogota/MapServer"
     ,{opacity: 0.6});
 
-      map.addLayer(fl2);  // adds the layer to the map
-
     var template = new PopupTemplate({
-        title: "School information {OBJECTID}",
+        title: "Información de la institución {OBJECTID}",
         description: "{PopupInfo}"
     });
 
-    var fl = new FeatureLayer("https://services2.arcgis.com/NFAaR7TquHkVlqVD/arcgis/rest/services/Colegios_Bogota/FeatureServer/0",{
-       outFields : ["*"],
-      infoTemplate: template
-    });
 
-   map.addLayer(fl);  // adds the layer to the map
+    map.addLayer(fl2);  // adds the layer to the map
+    map.addLayer(fl);  // adds the layer to the map
 
-    //var wms = new WMSLayer("http://serviciosgis.catastrobogota.gov.co/arcgis/services/Mapa_Referencia/Mapa_Referencia/MapServer/WMSServer?request=GetCapabilities&service=WMS"
-   // , {format: "png"});
+    //create a layer from csv file from drive
+   // var csv = new CSVLayer("https://drive.google.com/file/d/0BwvIXPDvHBvaNWgwdmgyczlySzA/view?usp=sharing",{
+    //   columnDelimiter: ";",
+    //    latitudeFieldName: "lat",
+    //    longitudeFieldName: "long"
+    //});
+    //var orangeRed = new Color([238, 69, 0, 0.5]);
+    //var marker = new SimpleMarkerSymbol("solid", 10, null, orangeRed);
 
-  //  wms.on("error", function (response){
-   //     console.log("Error: %s", response.error.message);
-   // });
+    // Establecemos el símbolo que se utilizará para
+    // representar los datos
+    //var renderer = new SimpleRenderer(marker);
+    //csv.setRenderer(renderer);
 
-    //map.addLayer(wms);
+    // Y asociamos un pequeño modal con información extra.
+    //var template = new InfoTemplate(
+    //   "Playa: ${name}",
+    //   "<iframe src='${pic_url}' frameborder='0' allowfullscreen style='width:100%'></iframe>",
+    //   "<a href='${thumb_url}' target='_blank'>Más info"
+    //);
+    //csv.setInfoTemplate(template);
+    //map.addLayer(csv);
 
     //wire event handlers to ensure that the layer which attempts to draw 1000 polygon graphics isn't visible while zooming
     fl.on("zoom-start", function() {
-        featureLayer.setVisibility(false);
+        fl.setVisibility(false);
     });
 
     fl.on("zoom-end", function() {
-        featureLayer.setVisibility(true);
+        fl.setVisibility(true);
     });
 
     fl2.on("zoom-start", function() {
-        featureLayer.setVisibility(false);
+        fl2.setVisibility(false);
     });
 
     fl2.on("zoom-end", function() {
-        featureLayer.setVisibility(true);
+        fl2.setVisibility(true);
     });
+
     // Basemaps
     query("#selectBasemapPanel").on("change", function(e){
         map.setBasemap(e.target.options[e.target.selectedIndex].value);
     });
 
+    //Search bar in a panel and in the nav
     var app = {};
     // Search
     app.searchDivNav = createSearchWidget("searchNavDiv");
@@ -99,5 +124,35 @@ require([
         search.startup();
         return search;
     }
+
+    //options to set the visibility of a layer
+    query("#layer1").on("change", function setLayerVisibility()
+    {
+        map.getLayer("layer1").setVisibility(!map.getLayer("layer1").visible);
+    });
+
+    query("#graphicsLayer2").on("change", function setLayerVisibility()
+    {
+        map.getLayer("graphicsLayer2").setVisibility(!map.getLayer("graphicsLayer2").visible);
+    });
+
+    //Sets initialVisibility of the layers in the checkboxes
+    function initialVisibility()
+    {
+        var layersIds = [];
+
+        layersIds = map.layerIds;
+        layersIds = layersIds.concat(map.graphicsLayerIds);
+        console.log(layersIds);
+        layersIds.forEach(function (l) {
+                if(l != "layer0")
+                {
+                    document.getElementById(l).checked = map.getLayer(l).visible;
+                }
+        });
+
+
+    }
+    initialVisibility();
 
 });
